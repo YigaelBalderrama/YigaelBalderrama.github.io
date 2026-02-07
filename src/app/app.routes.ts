a9f716989cc2061331290d74dbfbaef7d110d955
Yigael Balderrama <yigaelzero@gmail.com>
2026-02-07 00:08:20 -0400

    basic info added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeroParallaxComponent } from './components/hero-parallax/hero-parallax.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeroParallaxComponent,
+    title: 'Yigael Balderrama - Portafolio',
+  },
+];
--- src/app/components/hero-parallax/hero-parallax.component.ts
+++ src/app/components/hero-parallax/hero-parallax.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hero-parallax',
   imports: [],
   templateUrl: './hero-parallax.component.html',
-  styleUrl: './hero-parallax.component.scss'
+  styleUrl: './hero-parallax.component.scss',
 })
 export class HeroParallaxComponent {
+  scrollY = 0;
 
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrollY = window.scrollY;
+  }
+
+  t(speed: number) {
+    return `translateY(${this.scrollY * speed}px)`;
+  }
 }
